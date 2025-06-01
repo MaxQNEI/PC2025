72c767778c58ded97725ba5b020886e3368ee187
MaxQNEI <maxqnei@gmail.com>
2025-06-02 00:49:23 +0300

    Mv public -> docs

--- config.js
+++ config.js
@@ -4,4 +4,4 @@ export const PathRoot = import.meta.dirname;
 
 export const PathEntryPoint = PATH.resolve(PathRoot, "src", "app.js");
 
-export const PathOutfile = PATH.resolve(PathRoot, "public", "bundle.js");
+export const PathOutfile = PATH.resolve(PathRoot, "docs", "bundle.js");
