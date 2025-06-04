86e25c4d0c745d794d08f8c362549a472795ddb6
MaxQNEI <maxqnei@gmail.com>
2025-06-04 15:04:08 +0300

    Upd index: auto-update js/css timestamp

--- config.js
+++ config.js
@@ -4,4 +4,8 @@ export const PathRoot = import.meta.dirname;
 
 export const PathEntryPoint = PATH.resolve(PathRoot, "src", "app.js");
 
-export const PathOutfile = PATH.resolve(PathRoot, "docs", "bundle.js");
+export const PathDocs = PATH.resolve(PathRoot, "docs");
+
+export const PathOutfile = PATH.resolve(PathDocs, "bundle.js");
+
+export const PathIndexHTML = PATH.resolve(PathDocs, "index.html");
--- src/esbuild.js
+++ src/esbuild.js
@@ -1,6 +1,7 @@
 import esbuild from "esbuild";
 import esbuildSvelte from "esbuild-svelte";
-import { PathEntryPoint, PathOutfile } from "../config.js";
+import FSP from "fs/promises";
+import { PathDocs, PathEntryPoint, PathIndexHTML, PathOutfile } from "../config.js";
 
 const ctx = await esbuild.context({
     entryPoints: [PathEntryPoint],
@@ -12,3 +13,34 @@ const ctx = await esbuild.context({
 });
 
 await ctx.watch();
+
+(async () => {
+    try {
+        const update = ["bundle.css", "bundle.js"];
+        const watcher = FSP.watch(PathDocs, { recursive: false });
+
+        let updTimeout;
+        for await (const event of watcher) {
+            if (!update.includes(event.filename)) {
+                continue;
+            }
+
+            updTimeout && clearTimeout(updTimeout);
+            updTimeout = setTimeout(async () => {
+                const now = Date.now();
+                const html = (await FSP.readFile(PathIndexHTML, { encoding: "utf-8" })).replace(
+                    /"(bundle\.(js|css))(\?t=\d+)?"/g,
+                    `"$1?t=${now}"`,
+                );
+
+                await FSP.writeFile(PathIndexHTML, html, { encoding: "utf-8" });
+            }, 100);
+        }
+    } catch (err) {
+        if (err.name === "AbortError") {
+            return;
+        }
+
+        throw err;
+    }
+})();
